bb09d88ac7e5b52db04361f49cd6c3d54129259d
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-10 18:11:38 -0300

    feat: adicionando animacao ao selecionar um personagem

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,28 @@
-const selecaoSonic = document.querySelector("#botao-selecao-sonic")
-const selecaoShadow = document.querySelector("#botao-selecao-shadow")
-const personagem = document.querySelector("#personagem")
+const selecaoSonic = document.querySelector("#botao-selecao-sonic");
+const selecaoShadow = document.querySelector("#botao-selecao-shadow");
+const personagem = document.querySelector("#personagem");
 
 selecaoSonic.addEventListener("mouseover", () => {
+    personagem.classList.remove("opacidadeSaida");
+    personagem.classList.add("opacidadeEntrada");
     personagem.src = "./assets/img/personagens/sonic.png";
 })
 
 selecaoShadow.addEventListener("mouseover", () => {
+    personagem.classList.remove("opacidadeSaida");
+    personagem.classList.add("opacidadeEntrada");
     personagem.src = "./assets/img/personagens/shadow.png";
 })
+
+selecaoSonic.addEventListener("mouseout", () => {
+    personagem.classList.remove("opacidadeEntrada");
+    personagem.classList.add("opacidadeSaida");
+    personagem.src = "./assets/img/personagens/sonic.png";
+})
+
+selecaoShadow.addEventListener("mouseout", () => {
+    personagem.classList.remove("opacidadeEntrada");
+    personagem.classList.add("opacidadeSaida");
+    personagem.src = "./assets/img/personagens/shadow.png";
+})
+
