eb4bdb367a544051d5dd5c64b35ff516c16d3c99
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-10 19:00:52 -0300

    feat: criando funcao de pulo

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -5,6 +5,18 @@ function iniciarJogo(personagemEscolhido){
     } else{
         spritePersonagem.src = "./assets/img/sprites/shadow/shadow-correndo.png";
     }
+
+    document.addEventListener("keydown", (event) => { 
+        if(event.key === "ArrowUp"){
+            spritePersonagem.src = "./assets/img/sprites/sonic/sonic-pulando.png";
+            spritePersonagem.classList.add("pulo");
+            
+            setTimeout(() => {
+                spritePersonagem.classList.remove("pulo");
+                spritePersonagem.src = "./assets/img/sprites/sonic/sonic-correndo.gif";
+            }, 450)
+        }
+    })
     
 }
 
