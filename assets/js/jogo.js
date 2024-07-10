97ec059473bb91d6d2eaeff02b8b8fca93849f53
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-10 19:22:20 -0300

    fix: ajustando funcao de pular

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -1,5 +1,7 @@
 function iniciarJogo(personagemEscolhido){
     const spritePersonagem = document.querySelector("#sprite-personagem");
+    const spriteInimigo = document.querySelector("#sprite-inimigo");
+    
     if(personagemEscolhido == "Sonic"){
         spritePersonagem.src = "./assets/img/sprites/sonic/sonic-correndo.gif";
     } else{
@@ -8,16 +10,15 @@ function iniciarJogo(personagemEscolhido){
 
     document.addEventListener("keydown", (event) => { 
         if(event.key === "ArrowUp"){
-            spritePersonagem.src = "./assets/img/sprites/sonic/sonic-pulando.png";
+            spritePersonagem.src = "./assets/img/sprites/sonic/sonic-pulando.gif";
             spritePersonagem.classList.add("pulo");
             
             setTimeout(() => {
                 spritePersonagem.classList.remove("pulo");
                 spritePersonagem.src = "./assets/img/sprites/sonic/sonic-correndo.gif";
-            }, 450)
+            }, 800)
         }
     })
-    
 }
 
 export {iniciarJogo}
