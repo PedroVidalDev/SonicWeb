84bd5e5fb7fff3e6f834b6b6cf7e6d072520d24e
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-10 18:18:40 -0300

    feat: fazendo com que ao clickar chame o menu do jogo proprio, e definindo personagem selecionado

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,17 +1,15 @@
+const menu = document.querySelector("#menu")
+const jogo = document.querySelector("#jogo")
 const selecaoSonic = document.querySelector("#botao-selecao-sonic");
 const selecaoShadow = document.querySelector("#botao-selecao-shadow");
 const personagem = document.querySelector("#personagem");
+
+let personagemEscolhido;
 
 selecaoSonic.addEventListener("mouseover", () => {
     personagem.classList.remove("opacidadeSaida");
     personagem.classList.add("opacidadeEntrada");
     personagem.src = "./assets/img/personagens/sonic.png";
-})
-
-selecaoShadow.addEventListener("mouseover", () => {
-    personagem.classList.remove("opacidadeSaida");
-    personagem.classList.add("opacidadeEntrada");
-    personagem.src = "./assets/img/personagens/shadow.png";
 })
 
 selecaoSonic.addEventListener("mouseout", () => {
@@ -20,9 +18,28 @@ selecaoSonic.addEventListener("mouseout", () => {
     personagem.src = "./assets/img/personagens/sonic.png";
 })
 
+selecaoSonic.addEventListener("click", () => {
+    menu.style.display = "none";
+    jogo.style.display = "flex";
+    personagemEscolhido = "Sonic";
+})
+
+selecaoShadow.addEventListener("mouseover", () => {
+    personagem.classList.remove("opacidadeSaida");
+    personagem.classList.add("opacidadeEntrada");
+    personagem.src = "./assets/img/personagens/shadow.png";
+})
+
 selecaoShadow.addEventListener("mouseout", () => {
     personagem.classList.remove("opacidadeEntrada");
     personagem.classList.add("opacidadeSaida");
     personagem.src = "./assets/img/personagens/shadow.png";
 })
 
+selecaoShadow.addEventListener("click", () => {
+    menu.style.display = "none";
+    jogo.style.display = "flex";
+    personagemEscolhido = "Shadow";
+})
+
+export {personagemEscolhido};
